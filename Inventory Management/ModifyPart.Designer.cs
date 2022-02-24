68638f2d33f0275c093a094d10b3d4dac4b2914f
Chad <chad.lines1@gmail.com>
2022-02-24 11:13:34 -0700

    0.003 - Product Class Skeleton

--- Inventory Management/ModifyPart.Designer.cs
+++ Inventory Management/ModifyPart.Designer.cs
@@ -215,7 +215,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 327);
+            this.ClientSize = new System.Drawing.Size(336, 327);
             this.Controls.Add(this.CancelButton);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.textBox7);
---
+++ Inventory Management/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;    // Required for using BindingList
+
+namespace Inventory_Management
+{
+    internal class Product
+    {   
+        //public static BindingList<Part> parts = new BindingList<Part>()
+        public int ProductId { get; set; }  // The unique ID of the product
+        public string Name { get; set; }    // The name of the product
+        public decimal Price { get; set; }  // The product's price
+        public int InStock { get; set; }    // The number of products in stock
+        public int Min { get; set; }        // The minimum 
+        public int Max { get; set; }        // The maximum
+
+        public override string ToString()
+        {
+            // Overriding the default display to show useful information about a given Product
+            return "[ " + Name + " ] " + "Price: " + Price + ", Stock: " + InStock; 
+        }
+
+        //public void addAssociatedPart(Part part)
+        //{
+        //    return; 
+        //}
+        
+        public bool removeAssociatedPart(int id)
+        {
+            return false;
+        }
+
+        //public Part lookupAssociatedPart(int id)
+        //{
+        //    return Part;
+        //}
+    }
+}

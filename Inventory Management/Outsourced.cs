4b74cf8e6aa8a4ddac2b7b7b321f364fd92ccde4
Chad <chad.lines1@gmail.com>
2022-02-24 12:08:03 -0700

    0.006 - Inhouse and Outsourced Classes

---
+++ Inventory Management/Outsourced.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management
+{
+    public class Outsourced : Part
+    {
+        public string CompanyName { get; set; } // Outsourced parts have a company name
+    }
+}
